184b95712473830e3fc76f4f75ee4338f92bfff1
Sara Garza <sjgarza22@gmail.com>
2020-01-05 22:41:44 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     delimiters = [".\s", "!\s", "?\s"]
-    self.split(/[.\s | !\s | ?\s]/).length
+    self.split(Regexp.union(delimiters)).length
   end
 end
 
